c8c5af9eb326655b62f7c787b6b996e754c7f970
Liviru-Navaratna <livirunavaratna@gmail.com>
2024-10-11 22:22:10 +0530

    Fixed issues with the upload process
    Added a code line which was missing where the uploaded image won't be previewed. Also fixed the code to allow the image to be uploaded to the flask server to be processed

--- script.js
+++ script.js
@@ -49,6 +49,30 @@ function applyModeSettings() {
   };
 }
 
+//Image upload into web-page(drag and drop or upload)
+const dropArea = document.getElementById("drop-area-section");
+const inputFile = document.getElementById("input-file");
+const imageShow = document.getElementById("image-show");
+
+inputFile.addEventListener("change", uploadImage);
+
+function uploadImage(){
+    let imageLink =URL.createObjectURL(inputFile.files[0]);
+    imageShow.style.backgroundImage = `url(${imageLink})`;
+    imageShow.textContent = "";
+    imageShow.style.border = 0;
+}
+
+dropArea.addEventListener("dragover", function(e){
+    e.preventDefault();
+});
+
+dropArea.addEventListener("drop", function(e){
+    e.preventDefault();
+    inputFile.files = e.dataTransfer.files;
+    uploadImage();
+});
+
 // Proceed button
 const proceedButton = document.getElementById('proceed');
 proceedButton.addEventListener('click', async function (event) {
